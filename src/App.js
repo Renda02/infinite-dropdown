81f76beaba22a68d1daa98cc135db0434d3b084e
Renda02 <rendaniluvhengo89@gmail.com>
2021-02-15 15:41:55 +0100

    Added functionality to dropdown

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 
 import './App.css';
+import Dropdown from './Dropdown';
+
+
+const items = [
+  { id: 1, value: "Red" },
+  { id: 2, value: "Blue" },
+  { id: 3, value: "Purple" },
+  { id: 4, value: "Green" },
+];
 
 function App() {
   return (
-    <div className="App">
-   
+    <div className="wrapper">
+   <Dropdown title="Select color" items={items} multiSelect/>
     </div>
   );
 }
---
+++ src/Dropdown.js
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+import onClickOutside from 'react-onclickoutside'
+
+
+function Dropdown({ title, items = [], multiSelect = false }) {
+  const [open, setOpen] = useState(false);
+  const [select, setSelect] = useState([]);
+
+  const toggle = () => setOpen(!open);
+  Dropdown.handleClickOutside = () => setOpen(false);
+
+  function handleOnClick(item) {
+    if (!select.some((current) => current.id === item.id)) {
+      if (!multiSelect) {
+        setSelect([item]);
+      } else if (multiSelect) {
+        setSelect([...select, item]);
+      }
+    } else {
+      let selectAfterRemoval = select;
+      selectAfterRemoval = selectAfterRemoval.filter(
+        (current) => current.id !== item.id
+      );
+      setSelect([...selectAfterRemoval]);
+    }
+  }
+
+  function isItemInselect(item) {
+    if (select.some((current) => current.id === item.id)) {
+      return true;
+    }
+    return false;
+  }
+
+  return (
+    <div className="dropdown-wrapper">
+      <div
+        tabIndex={0}
+        className="dropdown-header"
+        role="button"
+        onKeyPress={() => toggle(!open)}
+        onClick={() => toggle(!open)}
+      >
+        <div className="dropdown-title">
+          <p className="dropdown-text">{title}</p>
+        </div>
+        <div className="dropdown-header__action">
+          <p className="dropdown-text">{open ? "Close" : "Open"}</p>
+        </div>
+      </div>
+      {open && (
+        <ul className="dropdown-list">
+          {items.map((item) => (
+            <li className="dropdown-list-item" key={item.id}>
+              <button type="button" onClick={() => handleOnClick(item)}>
+                <span>{item.value}</span>
+                <span>{isItemInselect(item) && "Selected"}</span>
+              </button>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+}
+
+const clickOutsideConfig = {
+    handleClickOutside: () => Dropdown.handleClickOutside,
+  };
+
+export default onClickOutside(Dropdown, clickOutsideConfig);
